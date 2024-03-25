294cee2d424358e4fea02f853daaab30075f1912
REDANE BADAOUI <103901647+RedaneDev@users.noreply.github.com>
2024-03-25 14:10:01 +0100

    Mise en place de EasyAdmin

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Author;
+use App\Entity\Book;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        return $this->redirect($adminUrlGenerator->setController(BookCrudController::class)->generateUrl());
+
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Api Books Test');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToCrud('Books', 'fas fa-book', Book::class);
+        yield MenuItem::linkToCrud('Authors', 'fas fa-user-pen', Author::class);
+    }
+}
